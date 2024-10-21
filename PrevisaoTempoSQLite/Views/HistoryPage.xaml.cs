a719d68c62f37c9f52c7c6b8f1ba9c11537e51ba
luiz166 <otavio.tata.morais@gmail.com>
2024-10-21 03:57:29 -0300

    handling listview data

--- PrevisaoTempoSQLite/Views/HistoryPage.xaml.cs
+++ PrevisaoTempoSQLite/Views/HistoryPage.xaml.cs
@@ -1,9 +1,26 @@
+using PrevisaoTempoSQLite.Models;
+using System.Collections.ObjectModel;
+
 namespace PrevisaoTempoSQLite.Views;
 
 public partial class HistoryPage : ContentPage
 {
+	ObservableCollection<Weather> list = new ObservableCollection<Weather>();
 	public HistoryPage()
 	{
 		InitializeComponent();
+
+		lst_weather.ItemsSource = list;
 	}
+
+    protected async override void OnAppearing()
+    {
+        List<Weather> temp = await App.Database.GetAll();
+        temp.ForEach(x => list.Add(x));
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
