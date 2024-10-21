c26555832472136ee54170be3f91fef0dc16b9be
luiz166 <otavio.tata.morais@gmail.com>
2024-10-21 02:47:13 -0300

    creation of routes

---
+++ PrevisaoTempoSQLite/Helpers/DbHelper.cs
@@ -0,0 +1,64 @@
+﻿using PrevisaoTempoSQLite.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrevisaoTempoSQLite.Helpers
+{
+    public class DbHelper
+    {
+        readonly SQLiteAsyncConnection conn;
+
+        public DbHelper(string path)
+        {
+            conn = new SQLiteAsyncConnection(path);
+            conn.CreateTableAsync<Weather>().Wait();
+
+        }
+
+        public Task<int> Insert(Weather weather) {
+            return conn.InsertAsync(weather);
+        }
+
+        public Task<List<Weather>> Update(Weather weather) {
+            string sql = "UPDATE  Weather " +
+                "SET Latitude=?, " +
+                "Longitude=?, " +
+                "City=?, " +
+                "County=?, " +
+                "Temperature=? WHERE Id=?";
+
+
+            return conn.QueryAsync<Weather>(sql, 
+                weather.Latitude,
+                weather.Longitude,
+                weather.City,
+                weather.County,
+                weather.Temperature,
+                weather.Id);
+
+        }
+
+        public Task<int> Delete(int id) {
+            return conn.Table<Weather>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<int> DeleteAll() {
+            return conn.DeleteAllAsync<Weather>();
+        }
+
+        public Task<List<Weather>> GetAll()
+        {
+            return conn.Table<Weather>().ToListAsync();
+        }
+
+        public Task<List<Weather>> Search(string query) {
+            string sql = "SELECT * FROM Weather WHERE city LIKE '%"+ query +"%'";
+
+            return conn.QueryAsync<Weather>(sql);
+        }
+    }
+}
