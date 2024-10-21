48753d696348942230973dc7b4ceaed74980ab04
luiz166 <otavio.tata.morais@gmail.com>
2024-10-21 17:21:05 -0300

    handle searchBar, handle delete by id, handle delete all

--- PrevisaoTempoSQLite/Views/HistoryPage.xaml.cs
+++ PrevisaoTempoSQLite/Views/HistoryPage.xaml.cs
@@ -19,8 +19,40 @@ public partial class HistoryPage : ContentPage
         temp.ForEach(x => list.Add(x));
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
     {
+        string query = e.NewTextValue;
 
+        list.Clear();
+
+        List<Weather> temp = await App.Database.Search(query);
+
+        temp.ForEach(x => list.Add(x));
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        var menuItem = (MenuItem)sender;
+        var itemId = (int)menuItem.CommandParameter;
+
+        await App.Database.Delete(itemId);
+        await Navigation.PushAsync(new HistoryPage());
+        Navigation.RemovePage(this);
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await App.Database.DeleteAll();
+
+            DisplayAlert("Sucesso", "Previsões deletadas", "Ok");
+            await Navigation.PushAsync(new HistoryPage());
+            Navigation.RemovePage(this);
+        }
+        catch(Exception ex)
+        {
+            DisplayAlert("Erro:", ex.Message, "Ok");
+        }
     }
 }
